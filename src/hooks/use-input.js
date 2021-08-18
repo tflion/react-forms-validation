76cd36c79237479c47e1c50f067bff305c9000eb
tflion <tfleao07@gmail.com>
2021-08-18 16:14:00 -0300

    useReducer-e-comentarios

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,27 +1,57 @@
-import { useState } from "react";
+import { useReducer } from "react";
+// import { useState } from "react";
+
+
+const initialInputstate = {
+    value: '',
+    isTouched: false
+}
+
+const inputStateReducer = (state, action) => {
+    if(action.type === 'INPUT'){
+        return{ value: action.value, isTouched: state.isTouched};
+    }
+    if(action.type === 'BLUR'){
+        return{isTouched: true, value: state.value}
+    }
+    if(action.type === 'RESET'){
+        return{ isTouched: false, value: ''}
+    }
+    
+    return inputStateReducer;
+}
 
 const useInput = (validateValue) => {
-    const [value, setValue] = useState('');
-    const [isTouched, setIsTouched] = useState(false);
+    const [inputState, dispatch] = useReducer(inputStateReducer, initialInputstate)
 
-    const valueIsValid = validateValue(value);
-    const hasError = !valueIsValid && isTouched;
+
+    // const [value, setValue] = useState('');
+    // const [isTouched, setIsTouched] = useState(false);
+
+    // const valueIsValid = validateValue(value);
+    // const hasError = !valueIsValid && isTouched;
+
+    const valueIsValid = validateValue(inputState.value);
+    const hasError = !valueIsValid && inputState.isTouched;
 
     const valueChangeHandler = (event) => {
-        setValue(event.target.value);
-
+        // setValue(event.target.value);
+        dispatch({ type: 'INPUT', value: event.target.value });
     }
     const inputBlurHandler = () => {
-        setIsTouched(true);
+        // setIsTouched(true);
+        dispatch({ type: 'BLUR'});
+
     }
 
-    const reset = () =>{
-        setValue('');
-        setIsTouched(false);
+    const reset = () => {
+        // setValue('');
+        // setIsTouched(false);
+        dispatch({type: 'RESET'})
     }
 
     return {
-        value,
+        value: inputState.value, // apenas value se for useState
         isValid: valueIsValid,
         hasError,
         valueChangeHandler,
